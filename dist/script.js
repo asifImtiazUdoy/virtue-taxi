7277d81ce66b45d7a3ec20189322ed3246772351
asifImtiazUdoy <am.asifudoy@gmail.com>
2024-10-04 19:25:11 +0600

    carousel responsive done

--- dist/script.js
+++ dist/script.js
@@ -1,28 +1,36 @@
 const swiper = new Swiper('.mySwiper', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: '.pagination',
-        type: 'fraction',
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
     },
-    navigation: {
-        nextEl: '.button-next',
-        prevEl: '.button-prev',
+    768: {
+      slidesPerView: 2,
     },
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
+    640: {
+      slidesPerView: 1,
     },
-    breakpoints: {
-        640: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 4
     },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 2
+    },
+  },
 });
